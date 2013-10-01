ef064983586f4817e35a6de70f3532dea08863e4
Cliffe Hodgkinson <cliffe@resounding.ca>
2013-10-01 06:24:31 -0400

    appointments list

--- resource/appointments.js
+++ resource/appointments.js
@@ -1,7 +1,16 @@
 var moment = require('moment')
 	cons = require('consolidate'),
     _ = require('underscore'),
-    util = require('util');
+    toViewModel = require('../public/js/util/toViewModel'),
+    util = require('util'),
+	path = require('path'),
+    fs = require('fs'),
+    _ = require('underscore'),
+    baseDir = path.join(__dirname, '../public/tmpl/followups'),
+    meetingHtml = fs.readFileSync(path.join(baseDir, 'meeting.html')).toString(),
+    meeting = _.template(meetingHtml, null, { variable: 'd' }),
+    phoneHtml = fs.readFileSync(path.join(baseDir, 'call.html')).toString(),
+    phone = _.template(phoneHtml, null, { variable: 'd' });
 
 function Appointments(db) {
 	return Appointments;
@@ -17,15 +26,154 @@ Appointments.new = function(req, res) {
 
 Appointments.show = function(req, res) {
 	var date = moment(req.params.appointment || null, 'YYYYMMDD') || moment(),
-		format = 'MMM DD';
+		format = 'dddd, MMM DD';
 
 	 if(!date.isSame(moment(), 'year')) {
 	 	format += ', YYYY'
 	 }
 
+
+	 var appointments = _.map([
+	 	{
+	 		firstName: 'Jim',
+	 		lastName: 'Smith',
+	 		organization: 'RBC Ancaster',
+	 		address: {
+	 			street: '654 Wilson St.',
+	 			city: 'Ancaster',
+	 			province: 'ON'
+	 		},
+	 		nextContact: {
+ 				confirmed: true,
+ 				type: 'meeting',
+	 			date: moment().hours(10).minutes(0).seconds(0).format(),
+	 			reason: 'New client - introduce to NexGen'
+	 		},
+	 		email: 'jim.smith@rbc.com',
+	 		phones: [
+	 			{ 
+	 				number: '(905) 648-5985' }
+	 		],
+	 		tags: [
+	 			'New Client'
+	 		]
+	 	},
+	 	{
+	 		firstName: 'Fred',
+	 		lastName: 'Johnson',
+	 		organization: 'Bick Financial',
+	 		address: {
+	 			street: '888 Wilson St.',
+	 			city: 'Ancaster',
+	 			province: 'ON'
+	 		},
+	 		nextContact: {
+ 				confirmed: true,
+ 				type: 'meeting',
+	 			date: moment().hours(11).minutes(0).seconds(0).format(),
+	 			reason: 'Discuss Turtle funds'
+	 		},
+	 		email: 'fred@bick.ca',
+	 		phones: [
+	 			{ 
+	 				number: '(905) 304-8876'
+	 			}
+	 		],
+	 		tags: [
+	 			'Turtle',
+	 			'Fall Roadshow'
+	 		]
+	 	},
+	 	{
+	 		firstName: 'Laurie',
+	 		lastName: 'Weston',
+	 		organization: 'London Life',
+	 		address: {
+	 			street: '48 Bay St.',
+	 			city: 'Mississauga',
+	 			province: 'ON'
+	 		},
+	 		nextContact: {
+ 				confirmed: true,
+ 				type: 'call',
+	 			date: moment().hours(11).minutes(30).seconds(0).format(),
+	 			reason: 'Interested in Fall Roadshow'
+	 		},
+	 		email: 'lweston@londonlife.com',
+	 		phones: [
+	 			{ 
+	 				number: '(800) 324-5588'
+	 			}
+	 		],
+	 		tags: [
+	 			'Div Inc',
+	 			'Fall Roadshow'
+	 		]
+	 	},
+	 	{
+	 		firstName: 'Paul',
+	 		lastName: 'Seguin',
+	 		organization: 'Edward Jones',
+	 		address: {
+	 			street: '88655 South Service Rd.',
+	 			city: 'Grimsby',
+	 			province: 'ON'
+	 		},
+	 		nextContact: {
+ 				confirmed: true,
+ 				type: 'call',
+	 			date: moment().hours(11).minutes(45).seconds(0).format(),
+	 			reason: 'Met at conference - following up'
+	 		},
+	 		email: 'paul.seguin@edwardjones.com',
+	 		phones: [
+	 			{ 
+	 				number: '(905) 578-0978'
+	 			}
+	 		],
+	 		tags: [
+	 			'Div Inc',
+	 			'Fall Roadshow'
+	 		]
+	 	},
+	 	{
+	 		firstName: 'Peter',
+	 		lastName: 'Clinton',
+	 		organization: 'Investment Planning Group',
+	 		address: {
+	 			street: '1444 Main St. W.',
+	 			city: 'Hamilton',
+	 			province: 'ON'
+	 		},
+	 		nextContact: {
+ 				confirmed: true,
+ 				type: 'meeting',
+	 			date: moment().hours(12).minutes(0).seconds(0).format(),
+	 			reason: 'Lunch meeting at 1010 Bistro'
+	 		},
+	 		email: 'pclinton@ipg.com',
+	 		phones: [
+	 			{ 
+	 				number: '(905) 525-2947'
+	 			}
+	 		],
+	 		tags: [
+	 			'Turtle',
+	 			'Fall Roadshow',
+	 			'Golf'
+	 		]
+	 	}
+	 ], function(appointment) {
+	 	var viewModel = toViewModel(appointment);
+
+	 	return (viewModel.nextContact.type == 'call' ? phone : meeting)(viewModel);
+	 });	 
+
 	res.render('appointments/show', {
 		date: date,
-		dateString: date.format(format)
+		dateString: date.format(format),
+		queryStringDate: req.params.appointment,
+		appointments: appointments
 	});
 }
 
