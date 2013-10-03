01d769714af7df1a43a678f5bd3920fa014a252c
Cliffe Hodgkinson <cliffe@resounding.ca>
2013-10-03 06:09:17 -0400

    search page

--- resource/search.js
+++ resource/search.js
@@ -1,6 +1,20 @@
 var util = require('util'),
     cons = require('consolidate'),
+    toViewModel = require('../public/js/util/toViewModel'),
+ 	path = require('path'),
+    fs = require('fs'),
+    _ = require('underscore'),
+    baseDir = path.join(__dirname, '../public/tmpl/people'),
+    personHtml = fs.readFileSync(path.join(baseDir, 'person.html')).toString(),
+    personTemplate = _.template(personHtml, null, { variable: 'd' }),
     database;
+
+function toHtml(doc) {
+    var viewModel = toViewModel(doc),
+    	html = personTemplate(viewModel);
+
+    return html;
+}
 
 function Search(db) {
 	database = db;
@@ -15,11 +29,32 @@ Search.index = function(req, res){
 		search = search.join('');
 	}
 
-	console.log('searching for ' + search + ', tag ' + tag);
+	var title = (search || tag) ? 'Searching ' : 'Search';
+	if(search) {
+		title += '"' + search + '"'
+	}
+
+	if(tag) {
+		title += search ? ' with tag ' : ' tag ';
+		title += '"' + tag + '"';
+	}
 
 	database.view('people/lastName', function(err, dbRes) {
+
+		if(tag) {
+			dbRes.forEach(function(row) {
+				row.tags = row.tags || [];
+				if(row.tags.indexOf(tag) === -1) {
+					row.tags.push(tag);
+				}
+			})
+		}
+
+		var rows = dbRes.map(toHtml);
+
 		res.render('people', {
-			people: dbRes
+			title: title,
+			people: rows
 		});  
 	});  
 };
